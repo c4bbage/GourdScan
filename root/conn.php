5570f219dc11f58cc8cca08a1cc4da0f8ded1454
c4bbage <c4bbage@qq.com>
2016-05-04 17:50:16 +0800

    fix bug

--- root/conn.php
+++ root/conn.php
@@ -1,6 +1,6 @@
 <?php
 $conn = mysqli_connect("127.0.0.1", 'root', 'usbw');
-mysqli_select_db( $conn，"pscan");
+mysqli_select_db( $conn,"pscan");
 session_start();
 if (@$_GET['hash']) {
     $_SESSION['hash'] = mysqli_real_escape_string($_GET['hash']);
