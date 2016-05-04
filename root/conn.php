7f1d0e6bd9dfb118bd385043855f23df1a5c4dc5
c4bbage <c4bbage@qq.com>
2016-05-04 17:56:54 +0800

    fix bug
    Procedural style string mysqli_real_escape_string ( mysqli $link ,
    string $escapestr )

--- root/conn.php
+++ root/conn.php
@@ -3,7 +3,7 @@ $conn = mysqli_connect("127.0.0.1", 'root', 'usbw');
 mysqli_select_db( $conn,"pscan");
 session_start();
 if (@$_GET['hash']) {
-    $_SESSION['hash'] = mysqli_real_escape_string($_GET['hash']);
+    $_SESSION['hash'] = mysqli_real_escape_string($conn,$_GET['hash']);
 }
 $hash = @$_SESSION['hash'];
 ?>
